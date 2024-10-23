31e2edd5a9ccf5ae3c8b0c89e2ae95acde670e1e
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-10-23 04:09:20 +0000

    Router Setup nd Usage

--- textify/src/App.js
+++ textify/src/App.js
@@ -1,60 +1,149 @@
-// import logo from './logo.svg';
+// // import logo from './logo.svg';
+
+// import "./App.css";
+// import Alert from "./components/Alert";
+// import Navbar from "./components/Navbar";
+// import TextForm from "./components/TextForm";
+// import About from "./components/About";
+// import React, { useState } from "react";
+// import { 
+//   BrowserRouter as Router, 
+//   Routes, 
+//   Route, 
+// } from "react-router-dom";
+
+// function App() {
+//   const [mode, setMode] = useState("light"); // whether the dark mode is enable or not
+
+//   const [alert, setAlert] = useState(null);
+//   const showAlert = (message, type) => {
+//     setAlert({
+//       msg: message,
+//       type: type,
+//     });
+
+//     setTimeout(() => {
+//       setAlert(null);
+//     }, 2000);
+//   };
+
+//   const toggleMode = () => {
+//     if (mode === "light") {
+//       setMode("dark");
+//       document.body.style.backgroundColor = "#0B192C";
+//       showAlert("Dark mode has been enabled", "success");
+//       document.title = "Textify - Dark Mode";
+
+//       //Flash Title -
+//       // setInterval(() => {
+//       //   document.title = 'Textify is Amazing App';
+//       // }, 2000);
+//       // setInterval(() => {
+//       //   document.title = 'Install Textify Now';
+//       // }, 1500);
+//     } else {
+//       setMode("light");
+//       document.body.style.backgroundColor = "white";
+//       showAlert("Light mode has been enabled", "success");
+//       document.title = "Textify - Light Mode";
+//     }
+//   };
+//   return (
+//     <>
+//       <Router>
+//         <Navbar
+//           title="TEXTIFY📝"
+//           home="Home"
+//           aboutText="About Textify"
+//           mode={mode}
+//           toggleMode={toggleMode}
+//         />
+//         <Alert alert={alert} />
+//         <div className="container my-3">
+
+//           <Routes>
+//             <Route path="/about">
+//               <About />
+//             </Route>
+
+//             <Route path="/">
+//               <TextForm
+//                 showAlert={showAlert}
+//                 heading="Enter your text and select the case you want to convert it to."
+//                 mode={mode}
+//               />
+//             </Route>
+//           </Routes>
+//         </div>
+//       </Router>
+
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
 import "./App.css";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-// import About from "./components/About";
-import React, { useState } from 'react';
-
-
+import About from "./components/About";
+import React, { useState } from "react";
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route
+} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');  // whether the dark mode is enable or not
+  const [mode, setMode] = useState("light"); // whether the dark mode is enable or not
 
-  const[alert, setAlert] = useState(null);
-  const showAlert = (message, type)=>{
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
 
-    setTimeout(() =>{
+    setTimeout(() => {
       setAlert(null);
     }, 2000);
-  }
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#0B192C';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#0B192C";
       showAlert("Dark mode has been enabled", "success");
-      document.title = 'Textify - Dark Mode';
-
-      // setInterval(() => {
-      //   document.title = 'Textify is Amazing App';
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = 'Install Textify Now';
-      // }, 1500);
-
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+      document.title = "Textify - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-      document.title = 'Textify - Light Mode';
+      document.title = "Textify - Light Mode";
     }
-  } 
+  };
+
   return (
     <>
-
-      <Navbar title="TEXTIFY📝" home='Home' aboutText="About Textify" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-      <TextForm showAlert={showAlert} heading="Enter your text and select the case you want to convert it to." mode={mode}/>
-      {/* <About/> */}
-      </div>
-
+      <Router>
+        <Navbar
+          title="TEXTIFY📝"
+          home="Home"
+          aboutText="About Textify"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter your text and select the case you want to convert it to." mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
